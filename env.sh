cfbf6764c706387d260063108138dfdd0f0a49ae
Jesse L. Zamora <xtremekforever@gmail.com>
2026-01-25 07:11:13 -0500

    Add ability to specify container runtime via DOCKER_CMD

--- env.sh
+++ env.sh
@@ -7,18 +7,29 @@
 DOCKER_IMAGE_TAG="meta-swift-project"
 DOCKER_WORKDIR="$PWD"
 
-# Yocto
+if [ -z "$DOCKER_CMD" ]; then
+  if command -v "docker ps" &> /dev/null; then
+    DOCKER_CMD="docker"
+  elif command -v podman ps &> /dev/null; then
+    DOCKER_CMD="podman"
+  else
+    echo "Error: Could not find installed or running 'docker' or 'podman'."
+    exit 1
+  fi
 
-YOCTO_DIR="$PWD"
+  echo "Found ${DOCKER_CMD}, this will be used as the container runtime."
+fi
 
-if command -v "docker ps" &> /dev/null; then
-  DOCKER_CMD="docker"
-elif command -v "podman ps" &> /dev/null; then
-  DOCKER_CMD="podman"
+if [ "$DOCKER_CMD" = "docker" ]; then
+  # Add extra params for docker if needed
+  DOCKER_RUN_PARAMS=""
+elif [ "$DOCKER_CMD" = "podman" ]; then
   DOCKER_RUN_PARAMS="--userns=keep-id:uid=$(id -u),gid=$(id -g)"
 else
-  echo "Error: Neither docker nor podman is installed or available."
+  echo "Error: Unsupported container runtime '${DOCKER_CMD}'."
   exit 1
 fi
 
-echo "Found ${DOCKER_CMD}, this will be used as the container runtime."
+# Yocto
+
+YOCTO_DIR="$PWD"
