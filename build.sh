ccdeb709cfa8d2515cb95aeaba5bcd0846859fc1
Jesse L. Zamora <xtremekforever@gmail.com>
2026-03-04 19:07:07 -0500

    Move builds, downloads, and sstate-cache into builds/ subdirectory

--- build.sh
+++ build.sh
@@ -2,17 +2,19 @@
 set -e
 
 # Configuration
+BUILD_ROOT=${BUILD_ROOT:=$(pwd)/builds}
 SRC_ROOT="${SRC_ROOT:=$(pwd)/sources}"
 POKY_DIR="${POKY_DIR:=$SRC_ROOT/poky}"
 META_SWIFT_DIR="${META_SWIFT_DIR:=$SRC_ROOT/meta-swift}"
 META_RASPBERRYPI_DIR=${META_RASPBERRYPI_DIR:=$SRC_ROOT/meta-raspberrypi}
 
 MACHINE="${MACHINE:=qemuarm}"
-BUILD_DIR=${BUILD_DIR:=build-$MACHINE}
-DOWNLOADS_DIR=${DOWNLOADS_DIR:=$(pwd)/downloads}
-SSTATE_DIR=${SSTATE_DIR:=$(pwd)/sstate-cache}
+BUILD_DIR=${BUILD_DIR:=$BUILD_ROOT/build-$MACHINE}
+DOWNLOADS_DIR=${DOWNLOADS_DIR:=$BUILD_ROOT/downloads}
+SSTATE_DIR=${SSTATE_DIR:=$BUILD_ROOT/sstate-cache}
 
 # Build Yocto Poky
+mkdir -p $BUILD_ROOT 2> /dev/null || true
 source $POKY_DIR/oe-init-build-env $BUILD_DIR
 bitbake-layers add-layer $META_SWIFT_DIR
 # Support for Raspberry PI devices
