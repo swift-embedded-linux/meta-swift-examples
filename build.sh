68e67d2c98bc1ed211fa364b481b495310ca909f
Jesse L. Zamora <xtremekforever@gmail.com>
2026-03-07 11:54:23 -0500

    Move submodules into sources/ subdirectory, store build folders at root (#12)

--- build.sh
+++ build.sh
@@ -2,18 +2,20 @@
 set -e
 
 # Configuration
-SRC_ROOT="${SRC_ROOT:=$(pwd)}"
+BUILD_ROOT=${BUILD_ROOT:=$(pwd)/builds}
+SRC_ROOT="${SRC_ROOT:=$(pwd)/sources}"
 POKY_DIR="${POKY_DIR:=$SRC_ROOT/poky}"
 META_SWIFT_DIR="${META_SWIFT_DIR:=$SRC_ROOT/meta-swift}"
 META_RASPBERRYPI_DIR=${META_RASPBERRYPI_DIR:=$SRC_ROOT/meta-raspberrypi}
 
 MACHINE="${MACHINE:=qemuarm}"
-DOWNLOADS_DIR=${DOWNLOADS_DIR:=$SRC_ROOT/downloads}
-SSTATE_DIR=${SSTATE_DIR:=$SRC_ROOT/sstate-cache}
+BUILD_DIR=${BUILD_DIR:=$BUILD_ROOT/build-$MACHINE}
+DOWNLOADS_DIR=${DOWNLOADS_DIR:=$BUILD_ROOT/downloads}
+SSTATE_DIR=${SSTATE_DIR:=$BUILD_ROOT/sstate-cache}
 
 # Build Yocto Poky
-cd $POKY_DIR
-source oe-init-build-env build-$MACHINE
+mkdir -p $BUILD_ROOT 2> /dev/null || true
+source $POKY_DIR/oe-init-build-env $BUILD_DIR
 bitbake-layers add-layer $META_SWIFT_DIR
 # Support for Raspberry PI devices
 if [[ $MACHINE == "raspberrypi"* ]]; then
@@ -33,5 +35,8 @@ echo "IMAGE_INSTALL:append = \" swift-hello-world ${EXTRA_IMAGE_INSTALL}\"" >> $
 #echo 'SSTATE_MIRRORS ?= "file://.* http://sstate.yoctoproject.org/all/PATH;downloadfilename=PATH"' >> $CONF_FILE
 #echo "USER_CLASSES += \"buildstats buildstats-summary\"" >> $CONF_FILE
 
-# build Swift
-bitbake core-image-minimal
+COMMAND="$1"
+COMMAND=${COMMAND:="bitbake core-image-minimal"}
+
+# run build command
+$COMMAND
