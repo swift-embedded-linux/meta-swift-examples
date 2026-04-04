c429455444e3796b550c634c8c0161cb95147ad5
Xaver Gruber <xaver.max.gruber@gmail.com>
2026-04-04 22:45:25 +0200

    remove unneeded build commands

--- clean-swift-build.sh
+++ clean-swift-build.sh
@@ -13,13 +13,5 @@ $SCRIPT_DIR/build.sh "bitbake -c cleansstate swift-xctest"
 $SCRIPT_DIR/build.sh "bitbake -c cleansstate swift-hello-world"
 $SCRIPT_DIR/build.sh "bitbake -c cleansstate core-image-minimal"
 
-$SCRIPT_DIR/build.sh "bitbake swift-stdlib"
-$SCRIPT_DIR/build.sh "bitbake libdispatch"
-$SCRIPT_DIR/build.sh "bitbake swift-foundation"
-$SCRIPT_DIR/build.sh "bitbake swift-foundation-essentials"
-$SCRIPT_DIR/build.sh "bitbake swift-foundation-icu"
-$SCRIPT_DIR/build.sh "bitbake swift-testing"
-$SCRIPT_DIR/build.sh "bitbake swift-xctest"
-$SCRIPT_DIR/build.sh "bitbake swift-hello-world"
 $SCRIPT_DIR/build.sh "bitbake core-image-minimal"
 
