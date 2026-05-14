ffed81170ec02c033aabae3242010250d8f7a760
Jesse L. Zamora <xtremekforever@gmail.com>
2026-05-14 13:39:22 -0400

    Add meta-yocto layer for beaglebone-yocto, fix build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 # Configuration
 BUILD_ROOT=${BUILD_ROOT:=$(pwd)/builds}
 SRC_ROOT="${SRC_ROOT:=$(pwd)/sources}"
-POKY_DIR="${POKY_DIR:=$SRC_ROOT/poky}"
+OE_DIR="${OE_DIR:=$SRC_ROOT/openembedded-core}"
 META_SWIFT_DIR="${META_SWIFT_DIR:=$SRC_ROOT/meta-swift}"
 META_RASPBERRYPI_DIR=${META_RASPBERRYPI_DIR:=$SRC_ROOT/meta-raspberrypi}
 
@@ -15,7 +15,9 @@ SSTATE_DIR=${SSTATE_DIR:=$BUILD_ROOT/sstate-cache}
 
 # Build Yocto Poky
 mkdir -p $BUILD_ROOT 2> /dev/null || true
-source $POKY_DIR/oe-init-build-env $BUILD_DIR
+source $OE_DIR/oe-init-build-env $BUILD_DIR
+bitbake-layers add-layer $SRC_ROOT/meta-yocto/meta-poky
+bitbake-layers add-layer $SRC_ROOT/meta-yocto/meta-yocto-bsp
 bitbake-layers add-layer $META_SWIFT_DIR
 # Support for Raspberry PI devices
 if [[ $MACHINE == "raspberrypi"* ]]; then
@@ -26,10 +28,10 @@ touch conf/sanity.conf
 CONF_FILE=./conf/local.conf
 rm -rf $CONF_FILE
 echo "# Swift for Yocto" >> $CONF_FILE
-echo "MACHINE=\"${MACHINE}\"" >> $CONF_FILE
+echo "MACHINE ??= \"${MACHINE}\"" >> $CONF_FILE
 echo "DL_DIR ?= \"${DOWNLOADS_DIR}\"" >> $CONF_FILE
 echo "SSTATE_DIR ?= \"${SSTATE_DIR}\"" >> $CONF_FILE
-echo 'IMAGE_FEATURES += "debug-tweaks"' >> $CONF_FILE
+echo 'EXTRA_IMAGE_FEATURES += "allow-empty-password empty-root-password allow-root-login post-install-logging"' >> $CONF_FILE
 echo "IMAGE_INSTALL:append = \" swift-hello-world ${EXTRA_IMAGE_INSTALL}\"" >> $CONF_FILE
 
 #echo 'SSTATE_MIRRORS ?= "file://.* http://sstate.yoctoproject.org/all/PATH;downloadfilename=PATH"' >> $CONF_FILE
