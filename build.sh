d7f7a535aa596c38efbba8ea063aabc3b07bb7f5
Jesse L. Zamora <xtremekforever@gmail.com>
2026-06-17 08:28:54 -0400

    Copy tmp/deploy output into output/build-$MACHINE directory
     - That way you don't have to dig into the volume to get it.
     - You'll still need to look in the volume for all the other build artifacts, but that can be done from a ./run-container.sh session.

--- build.sh
+++ build.sh
@@ -12,8 +12,7 @@ META_RASPBERRYPI_DIR=${META_RASPBERRYPI_DIR:=$SRC_ROOT/meta-raspberrypi}
 
 MACHINE="${MACHINE:=qemuarm}"
 BUILD_DIR=${BUILD_DIR:=$BUILD_ROOT/build-$MACHINE}
-DOWNLOADS_DIR=${DOWNLOADS_DIR:=$BUILD_ROOT/downloads}
-SSTATE_DIR=${SSTATE_DIR:=$BUILD_ROOT/sstate-cache}
+OUTPUT_DIR=${OUTPUT_DIR:=$(pwd)/output/build-$MACHINE}
 
 # For SWIFT_CXX_RUNTIME, we have 2 choices:
 # - "gnu" = libstdc++
@@ -56,3 +55,7 @@ COMMAND=${COMMAND:="bitbake core-image-minimal"}
 # run build command
 echo =============== BUILD COMMAND: $COMMAND
 $COMMAND
+
+echo "============== Copying build artifacts to output directory $OUTPUT_DIR"
+mkdir -p $OUTPUT_DIR 2> /dev/null || true
+cp -a $BUILD_DIR/tmp/deploy/* $OUTPUT_DIR 2> /dev/null || true
